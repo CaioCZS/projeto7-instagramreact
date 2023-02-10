4dae4f1004d6dc76f0365dc6da7014bac8dcbade
Caio <caiozs21@gmail.com>
2023-02-10 20:35:47 -0300

    criando Html com componentes

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import Corpo from "./Corpo";
+import NavBar from "./NavBar";
+
+
+export default function App() {
+    return (
+        <>
+            <div id="root">
+                <NavBar />
+                <Corpo />
+            </div>
+        </>
+    )
+}
